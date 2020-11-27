4c0f7bab2bcbda61b468c6af7e4d625eb7509cbf
Vadim <61653819+Exqzore@users.noreply.github.com>
2020-11-27 19:24:17 +0300

    Update AppConfig.cs

--- Source code/AppConfig.cs
+++ Source code/AppConfig.cs
@@ -2,10 +2,10 @@
 {
     public static class AppConfig
     {
-        public static string Key { get; set; } = "1364093115:AAEDOw8F4AA8n21aJ30Km3Gw6zgS6DU-jOE";
+        public static string Key { get; set; } = "API KEY";
 
-        public static string TelegramAddress { get; set; } = "https://t.me/zinpyr";
+        public static string TelegramAddress { get; set; } = "Telegram adress";
 
-        public static string VKAddress { get; set; } = "https://vk.com/vadikmen";
+        public static string VKAddress { get; set; } = "VK adress";
     }
 }
